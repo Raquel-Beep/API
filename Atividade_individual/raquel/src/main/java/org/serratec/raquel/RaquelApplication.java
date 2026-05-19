3e1f1f0456bc8ba4c07bf198a98e606c4b872370
Raquel-Bee <raquel.taveira26@gmail.com>
2026-05-19 14:35:31 -0300

    commit do spring initializr

---
+++ Atividade_individual/raquel/src/main/java/org/serratec/raquel/RaquelApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.raquel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaquelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaquelApplication.class, args);
+	}
+
+}
---
+++ Atividade_individual/raquel/src/test/java/org/serratec/raquel/RaquelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.raquel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaquelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
