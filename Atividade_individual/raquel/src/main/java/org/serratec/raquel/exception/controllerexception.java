3e38d16855cea6214267e321a4c7711593eaed84
Raquel-Bee <raquel.taveira26@gmail.com>
2026-05-20 22:48:27 -0300

    Adicionar classe ControllerException para tratamento de erros

---
+++ Atividade_individual/raquel/src/main/java/org/serratec/raquel/exception/controllerexception.java
@@ -0,0 +1,45 @@
+package org.serratec.raquel.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+ public class controllerexception  extends responseentityexceptionhandler {
+
+    @Override
+    protected ResponseEntity<Object> handlehttpmensagenotreadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        String mensagem = "Requisição mal formatada: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(erro ->
+            erros.put(erro.getField(), erro.getDefaultMessage())
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erros);
+    }
+
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("Ocorreu um erro: " + ex.getMessage());
+    }
+}
